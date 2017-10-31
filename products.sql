39a3be7437eba7a11458819162c62286d59b1084
Kaetee Pang <kaeteepang@Kaetees-MacBook-Pro.local>
2017-10-30 18:37:56 -0700

    starting

---
+++ products.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS bamazonDB;
+CREATE DATABASE bamazonDB;
+USE bamazonDB;
+
+CREATE TABLE products (
+	-- item_id (unique id)
+	id INT NOT NULL,
+	-- product_name
+	product_name VARCHAR (100) NOT NULL,
+	-- department_name 
+	department_name VARCHAR(100) NOT NULL,
+	-- price (cost to customer)
+	price FLOAT (100) NOT NULL,
+	-- stock_quantity (how much available in stores)
+	stock_quantity INT (1000) NOT NULL,
+
+	PRIMARY KEY(id)
+);
+
